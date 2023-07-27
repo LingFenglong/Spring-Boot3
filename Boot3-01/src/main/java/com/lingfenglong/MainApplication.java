6a4d5613ed5ca8e82621cda29b7edb753b9a4570
LingFenglong <2808021998@qq.com>
2023-07-27 22:00:25 +0800

    Initial commit

---
+++ Boot3-01/src/main/java/com/lingfenglong/MainApplication.java
@@ -0,0 +1,12 @@
+package com.lingfenglong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 启动SpringBoot的主入口程序
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ Boot3-02/src/test/java/com/lingfenglong/boot302/Boot302ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingfenglong.boot302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
