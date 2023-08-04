176cb4e787b6c033c56938b3cc26e0ec6eaab6f6
LingFenglong <2808021998@qq.com>
2023-08-04 15:55:09 +0800

    all

---
+++ Boot3-14-actuator/src/main/java/com/lingfenglong/actuator/Boot314ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.lingfenglong.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot314ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot314ActuatorApplication.class, args);
+	}
+
+}
